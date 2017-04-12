abb0b5d5e34def177a3835bac11b22b8c555bda2
Lukas Wolfsteiner <lukas@wolfsteiner.de>
2017-04-12 11:51:08 +0200

    Definiere Main-Funktion zum Testen des Programms

---
+++ blatt_02/pixel-tetris/main.cpp
@@ -0,0 +1,15 @@
+#include "libs/Field.h"
+#include "libs/Renderer.h"
+
+int main() {
+    Field field;
+    Stone stone;
+    Renderer renderer;
+
+    renderer.addObject(field);
+    renderer.addObject(stone);
+
+    renderer.Render();
+
+    return 0;
+}
