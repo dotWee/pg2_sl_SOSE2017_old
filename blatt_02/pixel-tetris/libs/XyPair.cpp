820e592949e8b3afb56a5f022b66c993664e6d3b
Lukas Wolfsteiner <lukas@wolfsteiner.de>
2017-04-20 10:11:44 +0200

    Entferne setter-Methoden für x & y; erlaube nur Definitionen durch den Constructor

--- blatt_02/pixel-tetris/libs/XyPair.cpp
+++ blatt_02/pixel-tetris/libs/XyPair.cpp
@@ -8,16 +8,11 @@ int XyPair::getX() {
     return x;
 }
 
-void XyPair::setX(int x) {
-    XyPair::x = x;
-}
-
 int XyPair::getY() {
     return y;
 }
 
-void XyPair::setY(int y) {
-    XyPair::y = y;
+XyPair::XyPair(int x, int y) {
+    this->x = x;
+    this->y = y;
 }
-
-XyPair::XyPair(int x, int y) : setX(x), setY(y) {}
--- blatt_02/pixel-tetris/libs/XyPair.h
+++ blatt_02/pixel-tetris/libs/XyPair.h
@@ -13,11 +13,7 @@ public:
 
     int getX();
 
-    void setX(int x);
-
     int getY();
-
-    void setY(int y);
 
 private:
     int x;
