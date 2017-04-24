c52db56e541c0f8536d9a273a95be29af1b6f59f
Lukas Wolfsteiner <lukas@wolfsteiner.de>
2017-04-24 17:08:36 +0200

    Füge Funktion-/Parameter Annotations als Erklärung hinzu

--- blatt_02/pixel-tetris/libs/Stone.h
+++ blatt_02/pixel-tetris/libs/Stone.h
@@ -13,21 +13,45 @@
 class Stone {
 
 private:
+
+    // Enthält Daten über Form des Spielsteines
     char buffer[STONE_DIMENSION_DEFAULT][STONE_DIMENSION_DEFAULT];
+
+    // Position als Offset im Spielfeld
     XyPair pos = XyPair(0, 0);
+
+    // Size als Größe der Dimension des Arrays Buffer
     XyPair size = XyPair(STONE_DIMENSION_DEFAULT, STONE_DIMENSION_DEFAULT);
 
 public:
     Stone();
 
+    /**
+     * @return X-Wert der XyPair Variable pos
+     */
     int pos_x();
 
+    /**
+     * @return Y-Wert der XyPair Variable pos
+     */
     int pos_y();
 
+    /**
+     * @return X-Wert der XyPair Variable size
+     */
     int size_x();
 
+    /**
+     * @return Y-Wert der XyPair Variable size
+     */
     int size_y();
 
+    /**
+     * Gibt Inhalt des Buffers anhand XyPair-Koordinaten zurück
+     * @param coord Position im Buffer
+     *
+     * @return Inhalt des Buffers
+     */
     char getBuffer(XyPair coord);
 };
 
