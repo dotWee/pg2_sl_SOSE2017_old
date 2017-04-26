2fcf61c377404e0100344335856da76996e44f17
Lukas Wolfsteiner <lukas@wolfsteiner.de>
2017-04-26 12:19:05 +0200

    Gib Anzahl der Fehler pro Klasse aus anstelle sie zu addieren

--- blatt_02/pixel-tetris/main_test.cpp
+++ blatt_02/pixel-tetris/main_test.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include "libs/Field.h"
 #include "test/FieldTest.h"
 #include "test/StoneTest.h"
@@ -8,15 +9,15 @@ int main() {
     // Teste Field-Klasse
     Field field;
     FieldTest fieldTest(field);
-    int errors_field = 0;
-    errors_field += fieldTest.test();
+    int errors_field = fieldTest.test();
+    std::cout << "Fehlerzahl der Field-Klasse: " << errors_field << "\n";
     errors += errors_field;
 
     // Teste Stone-Klasse
     Stone stone;
     StoneTest stoneTest(stone);
-    int errors_stone = 0;
-    errors_stone += stoneTest.test();
+    int errors_stone = stoneTest.test();
+    std::cout << "Fehlerzahl der Stone-Klasse: " << errors_stone << "\n";
     errors += errors_stone;
 
     return errors;
